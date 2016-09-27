085ca75bb5c7f58bf71d7e176d49727ec3e51135
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-27 13:18:43 +0800

    添加基本内容

--- v2/src/command.base.js
+++ v2/src/command.base.js
@@ -1,6 +1,8 @@
 (function(){
     'use strict';
 
+    this.variables = {};
+
     var commands = {};
     commands.list = {};
     commands.extend = function(ex){
--- v2/src/command.math.js
+++ v2/src/command.math.js
@@ -1,8 +1,34 @@
 (function(){
-    this.extend({
+    'use strict';
+
+    var commands = this.commands;
+    var variables = this.variables;
+
+    commands.extend({
         name : 'print',
-        test : function(a) { return /^(print|pr) .*$/.test(a); },
-        method : function(a) { return a; },
-        param : function(a) { return a.match(/^(print|pr) (.*)$/)[2]; }
+        test : (a)=> { return /^(print|pr) .*$/.test(a); },
+        method : (a) => { return a; },
+        param : (a) => { return a.match(/^(print|pr) (.*)$/)[2]; }
     });
-}).call(this.commands);
+
+    commands.extend({
+        name : 'output',
+        test : (a) => { return /^(output|op) .*$/.test(a); },
+        method : (a) => { return variables[a]; },
+        param : (a) => { return a.match(/^(output|op) (.*)$/)[2]; } 
+    });
+
+    commands.extend({
+        name : 'int',
+        test : (a) => { return /^int .*$/.test(a); },
+        method : (a) => {
+            if(/^(\d|\.)*$/.test(a)){
+                return Math.floor(parseFloat(a));
+            }
+            else{
+                return Math.floor(parseFloat(variables[a]));
+            };
+        },
+        param : (a) => { return a.match(/^int (\w*|(\d|\.)*)$/)[1]; }
+    });
+}).call(this);
