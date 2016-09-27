0a4363ff4cf0ff2079fe3443ae7381ea1905c59d
Gscienty <gaoxiaochuan@hotmail.com>
2016-09-27 22:58:31 +0800

    部分字符串

--- v2/src/command.string.js
+++ v2/src/command.string.js
@@ -9,10 +9,49 @@
         name : 'first',
         test : (a) => { return a.match(/^first /); },
         method : (a) => {
-            if(a.name === 'string'){
-                
+            if(a.name === 'string' && a.content.length > 2){
+                return { name : 'string', content : '\'' + a.content[1] + '\'' };
             };
+            return { name : 'string', content : '\'\'' };
         },
         param : 1
     });
+
+    commands.extend({
+        name : 'last',
+        test : (a) => { return a.match(/^last /); },
+        method : (a) => {
+            if(a.name === 'string' && a.content.length > 2){
+                return { name : 'string', content : '\'' + a.content[a.content.length - 2] + '\'' };
+            };
+            return { name : 'string', content : '\'\'' }
+        },
+        param : 1
+    });
+
+    commands.extend({
+        name : 'butfirst',
+        test : (a) => { return a.match(/^(butfirst|bf) /); },
+        param : 1,
+        method : (a) => {
+            if(a.name === 'string' && a.content.length > 2){
+                return { name : 'string', content : '\'' + a.content.substr(2, a.content.length - 3) + '\'' };
+            };
+            return { name : 'string', content : '\'\'' }
+        }
+    });
+
+    commands.extend({
+        name : 'butlast',
+        test : (a) => { return a.match(/^(butlast|bl) /); },
+        param : 1,
+        method : (a) => {
+            if(a.name === 'string' && a.content.length > 2){
+                return { name : 'string', content : '\'' + a.content.substr(1, a.content.length - 3) + '\'' };
+            };
+            return { name : 'string', content : '\'\'' }
+        }
+    });
+
+    
 }).call(this);
